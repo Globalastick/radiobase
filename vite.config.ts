4c603793b120c97815c68300a267ed0c0d0ed888
Globalastick <globalastick@gmail.com>
2024-09-23 12:51:12 +0300

    feat: add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,39 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [
+		react(),
+		svgr(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'RADIOBASE',
+				short_name: 'RADIOBASE',
+				description: 'Application for listening to Internet radio stations',
+				display: 'standalone',
+				scope: '/radiobase',
+				start_url: '/radiobase',
+				theme_color: '#f7e7d9',
+				background_color: '#f7e7d9',
+				icons: [
+					{
+						src: 'logo-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'logo-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
 	base: '/radiobase',
 	resolve: {
 		alias: {
